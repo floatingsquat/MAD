9ee9791083981da510e984bedba8e9c077755457
370709071 <muratyilmaz1634@gmail.com>
2020-11-23 00:16:33 +0300

    lab6

---
+++ Lab6/app/src/main/java/com/example/dynamicfragment/DetailsActivity.java
@@ -0,0 +1,23 @@
+package com.example.dynamicfragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.dynamicfragment.dummy.DetailsFragment;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        Movie movie = getIntent().getParcelableExtra("movie");
+
+        FragmentTransaction fts = getSupportFragmentManager().beginTransaction();
+        fts.add(R.id.container, DetailsFragment.newInstance(movie));
+        fts.commit();
+    }
+}
